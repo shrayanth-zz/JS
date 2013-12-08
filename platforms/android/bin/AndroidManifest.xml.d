e59b348bd1c28c2dbd65cc17c24a0ef9f903bbc8
Shrayanth Kumar <shrayanth@gmail.com>
2013-12-08 17:20:39 +0530

    init

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\nfctest\java_js\hello\platforms\android\bin\AndroidManifest.xml : \
+C:\nfctest\java_js\hello\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\nfctest\java_js\hello\platforms\android\gen\com\example\jajs\R.java \
+ : C:\nfctest\java_js\hello\platforms\android\res\drawable\icon.png \
+C:\nfctest\java_js\hello\platforms\android\res\drawable-hdpi\icon.png \
+C:\nfctest\java_js\hello\platforms\android\res\drawable-ldpi\icon.png \
+C:\nfctest\java_js\hello\platforms\android\res\drawable-mdpi\icon.png \
+C:\nfctest\java_js\hello\platforms\android\res\drawable-xhdpi\icon.png \
+C:\nfctest\java_js\hello\platforms\android\res\values\strings.xml \
+C:\nfctest\java_js\hello\platforms\android\res\xml\config.xml \
+C:\nfctest\java_js\hello\platforms\android\bin\AndroidManifest.xml \
